2af14b63cb55d70a6c15b59281933edf9851ab1e
ujjwal230701 <mani.bansalpkl@gmail.com>
2026-03-09 02:24:22 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://snjnphnxhoucvlnryqlb.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_dJfN_HUndUWPq6440yogVA_RfK4XGGY";
+export const ADMIN_PASSWORD = "cic2026admin"; // ← change this!
+export const CATEGORIES = ["Chairs & Seating", "Desks & Tables", "Storage & Shelving", "Accessories & Misc"];
